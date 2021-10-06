bcbe6c4ffc0a51c7efd82332f00373650816729d
Justis Lamanna <lamanna.justis@gmail.com>
2021-10-06 17:50:03 -0500

    Make it so backend runs frontend

--- PNGTuber-Backend/Startup.cs
+++ PNGTuber-Backend/Startup.cs
@@ -56,6 +56,19 @@ namespace PNGTuber
                 endpoints.MapHub<VoiceHub>("/voices");
                 endpoints.MapControllers();
             });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "../PNGTuber/ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
         }
     }
 }
